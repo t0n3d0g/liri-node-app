6a8595d0c4ba0a249a390f329643c14839a10726
t0n3d0g <utexas@excavata.com>
2018-11-26 09:42:17 -0600

    Updated latest homework files

---
+++ liri.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+
+
+// LIRI is a Language Interpretation and Recognition Interface.
+// LIRI will be a command line node app
+// that takes in parameters and gives you back data.
+
+
+// Example: concert-this <artist/band name> returns
+// Name of the venue, Venue location, Date of the Event
+// (use moment to format this as "MM/DD/YYYY")
+
+// Example: spotify-this-song '<song name here>' returns
+// Artist(s), song name, preview link, album
+
+// Example: movie-this '<movie name here>' returns
+// Title, Year, IMDB Rating, Rotten Tomatoes Rating,
+// Country, Language, Plot, Actors
+
+// Example: do-what-it-says
+
