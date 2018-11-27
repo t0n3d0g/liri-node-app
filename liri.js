24d6dfcd024bf6d4ad51746dd3d314b48f6a6310
t0n3d0g <utexas@excavata.com>
2018-11-27 06:49:08 -0600

    Updated with all 3 modules working

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
+// LIRI is a Language Interpretation and Recognition Interface.
+// LIRI will be a command line node app
+// that takes in parameters and gives you back data.
+
+// These are required to run the program
+
 require("dotenv").config();
 const axios = require("axios");
 const Spotify = require("node-spotify-api");
@@ -6,8 +12,13 @@ const moment = require("moment");
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
 
+// Inputs user command and info for Liri
+
 var command = process.argv[2];
 var info = process.argv.slice(3).join(" ");
+
+// Switch was introduced by my tutor to simplify choosing between
+// various functions including cases where a request has no attributes
 
 switch(command){
     case "spotify-this-song":
@@ -22,16 +33,15 @@ switch(command){
     } else {console.log ("Please select an Artist to search for their Concerts")}
     break;
 
+    case "movie-this":
+    if(info){
+    movieSearch(info);
+    } else {movieSearch("Mr. Nobody")}
+    break;
+
     default:
     console.log("default state");
 }
-
-
-console.log(info);
-
-// LIRI is a Language Interpretation and Recognition Interface.
-// LIRI will be a command line node app
-// that takes in parameters and gives you back data.
 
 // Example: concert-this <artist/band name> returns
 // Name of the venue, Venue location, Date of the Event
@@ -41,15 +51,12 @@ function concertSearch (band) {
     var concertQuery = "https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp";
    
     axios.get(concertQuery).then(function(res, err){
-        // For each response object, perform this function
         res.data.forEach(function(concert){
-            // Converting date to my format.
+            // Converting date
             var concertDate = concert.datetime.split("T");
-            var momentDate = moment(concertDate[0]).format("DDMMMYYYY");
+            var momentDate = moment(concertDate[0]).format("MM/DD/YYYY");
         
-            //  Output to console.log requested results for Artist
-            // console.log("VENUE: "+data.concert.venue.name);
- 
+            // Output to console.log
             console.log("--------------------------")
             console.log("VENUE: "+concert.venue.name);
             console.log("LOCATION: "+concert.venue.city);
@@ -80,5 +87,24 @@ function songSearch (title) {
 // Example: movie-this '<movie name here>' returns
 // Title, Year, IMDB Rating, Rotten Tomatoes Rating,
 // Country, Language, Plot, Actors
+
+function movieSearch (title) {
+
+    var movieQuery = "http://www.omdbapi.com/?t="+title+"&y=&plot=short&apikey=trilogy";
+
+    axios.get(movieQuery).then(function(res){
+        // Output to console.log
+        console.log("TITLE: "+res.data.Title);
+        console.log("YEAR: "+res.data.Year);
+        console.log("IMDB Rating: "+res.data.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: "+res.data.Ratings[1].Value);
+        console.log("COUNTRY: "+res.data.Country);
+        console.log("LANGUAGE: "+res.data.Language);
+        console.log("PLOT: "+res.data.Plot);
+        console.log("ACTORS: "+res.data.Actors);
+    
+    });
+}
+
 
 // Example: do-what-it-says
