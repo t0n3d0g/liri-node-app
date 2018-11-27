663473bb9db509dc04962c9d0ca30eb2324603b5
t0n3d0g <utexas@excavata.com>
2018-11-27 06:14:50 -0600

    Updated with 2 processes working successfully

--- liri.js
+++ liri.js
@@ -1,22 +1,84 @@
 require("dotenv").config();
+const axios = require("axios");
+const Spotify = require("node-spotify-api");
+const moment = require("moment");
+
+const keys = require("./keys.js");
+const spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var info = process.argv.slice(3).join(" ");
+
+switch(command){
+    case "spotify-this-song":
+    if(info){
+    songSearch(info);
+    } else {songSearch("Ace of Spades")}
+    break;
+
+    case "concert-this":
+    if(info){
+        concertSearch(info);
+    } else {console.log ("Please select an Artist to search for their Concerts")}
+    break;
+
+    default:
+    console.log("default state");
+}
 
 
+console.log(info);
 
 // LIRI is a Language Interpretation and Recognition Interface.
 // LIRI will be a command line node app
 // that takes in parameters and gives you back data.
 
-
 // Example: concert-this <artist/band name> returns
 // Name of the venue, Venue location, Date of the Event
 // (use moment to format this as "MM/DD/YYYY")
 
+function concertSearch (band) {
+    var concertQuery = "https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp";
+   
+    axios.get(concertQuery).then(function(res, err){
+        // For each response object, perform this function
+        res.data.forEach(function(concert){
+            // Converting date to my format.
+            var concertDate = concert.datetime.split("T");
+            var momentDate = moment(concertDate[0]).format("DDMMMYYYY");
+        
+            //  Output to console.log requested results for Artist
+            // console.log("VENUE: "+data.concert.venue.name);
+ 
+            console.log("--------------------------")
+            console.log("VENUE: "+concert.venue.name);
+            console.log("LOCATION: "+concert.venue.city);
+            console.log("REGION: "+concert.venue.region);
+            console.log("COUNTRY: "+concert.venue.country);
+            console.log("DATE: "+momentDate);    });
+    });            
+   
+    
+} 
+
 // Example: spotify-this-song '<song name here>' returns
 // Artist(s), song name, preview link, album
+
+function songSearch (title) {
+    spotify.search({ type: 'track', query: title }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+    //  Output to console.log requested results for song
+      console.log("ARTIST NAME: "+data.tracks.items[0].artists[0].name);
+      console.log("TRACK NAME: "+data.tracks.items[0].name);
+      console.log("PREVIEW URL: "+data.tracks.items[0].preview_url);
+      console.log("ALBUM: "+data.tracks.items[0].album.name);
+      });} 
+
 
 // Example: movie-this '<movie name here>' returns
 // Title, Year, IMDB Rating, Rotten Tomatoes Rating,
 // Country, Language, Plot, Actors
 
 // Example: do-what-it-says
-
